f76dfff046923759926fbc5e0ab8b90fe8adf54b
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-21 19:55:49 +0530

    Floyd's Triangle

---
+++ Patterns/Patterns/Floyds_Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Floyds_Triangle
+    {
+        public static void Main()
+        {
+            int n = 4;
+            int count = 1;
+
+            for(int i = 1; i<=n; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write(count++ + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
