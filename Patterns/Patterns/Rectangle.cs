f64cfc5457c0afe71e253111cb5e45bfdd776027
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-21 15:56:38 +0530

    Solid Half Pyramid and Rectangle Pattern

---
+++ Patterns/Patterns/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Rectangle
+    {
+        public static void Main()
+        {
+            int n = 4;
+            int cols = 6;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= cols; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Patterns/Patterns/Solid_Half_Pyramid_With_Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Solid_Half_Pyramid_With_Numbers
+    {
+        public static void Main()
+        {
+            int n = 4;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
