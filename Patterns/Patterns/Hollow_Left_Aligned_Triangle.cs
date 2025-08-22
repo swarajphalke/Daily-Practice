0f688cea64fa2ab4535392ee6f5235ae1895dab6
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-22 19:18:45 +0530

    Hollow Left and Right Aligned Triangle

---
+++ Patterns/Patterns/Hollow_Left_Aligned_Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Hollow_Left_Aligned_Triangle
+    {
+        public static void Main()
+        {
+            int n = 4;
+            //int count = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || j == i || i == n)
+                    {
+                        Console.Write("* ");
+                        //Console.Write(count++ + " ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Patterns/Patterns/Hollow_Right_Aligned_Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Hollow_Right_Aligned_Triangle
+    {
+        public static void Main()
+        {
+            int n = 4;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int s = 1; s <= n - i; s++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    if (j == 1 || i == j || i == n)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
