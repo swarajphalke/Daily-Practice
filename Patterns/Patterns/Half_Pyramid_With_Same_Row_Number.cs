8e23149111663226f2da16883dc6bc76a2db5275
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-22 19:16:02 +0530

    Half Pyramid with Same Row Number

---
+++ Patterns/Patterns/Half_Pyramid_With_Same_Row_Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Half_Pyramid_With_Same_Row_Number
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
