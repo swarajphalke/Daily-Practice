5dd36ad7266a3a46d74b1a433a72d588bd956686
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-22 19:19:42 +0530

    Continuous Alphabet Triangle

---
+++ Patterns/Patterns/Continuous_Alphabet_Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Continuous_Alphabet_Triangle
+    {
+        public static void Main()
+        {
+            int n = 5;
+            char letter = 'A';
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(letter++ + " ");
+                    //letter++
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
