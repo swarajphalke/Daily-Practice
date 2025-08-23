c424e4ec63ae161ea321b33e6d2c1cfc071c8423
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-23 18:23:58 +0530

    Alphabet Diamond and Rhombus Pattern

---
+++ Patterns/Patterns/Alphabet_Diamond.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Alphabet_Diamond
+    {
+        public static void Main()
+        {
+            int n = 5;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int s = 1; s <= n - i; s++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write((char)(64 + j) + " ");
+                }
+                for (int j = i - 1; j >= 1; j--)
+                {
+                    Console.Write((char)(64 + j) + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int s = 1; s <= n - i; s++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write((char)(64 + j) + " ");
+                }
+                for (int j = i - 1; j >= 1; j--)
+                {
+                    Console.Write((char)(64 + j) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Patterns/Patterns/Rhombus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Rhombus
+    {
+        public static void Main()
+        {
+            int n = 4;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int s = 1; s <= n - i; s++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
