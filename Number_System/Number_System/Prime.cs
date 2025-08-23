9a376a05b55617de437c65d9f45909cdb8c0f58e
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-23 18:24:13 +0530

    Prime Number

---
+++ Number_System/Number_System/Prime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Number_System;
+namespace Number_System
+{
+    internal class Prime
+    {
+        public void CheckPrime(int number)
+        {
+            int count = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine(number + " is Prime");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not Prime");
+            }
+
+        }
+        public static void Main()
+        {
+            Prime p = new Prime();
+            p.CheckPrime(13);
+        }
+    }
+}
---
+++ Number_System/Number_System/PrimeNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_System
+{
+    internal class PrimeNumbers
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                int number = i;
+                int count = 0;
+
+                for (int j = 1; j <= number; j++)
+                {
+                    if (number % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(number + " is Prime ");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not Prime ");
+                }
+            }
+        }
+    }
+}
