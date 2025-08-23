e0cc2e740b187b0f21bbd732febdf42507468244
Swaraj Phalke <swarajphalke555@gmail.com>
2025-08-23 18:26:34 +0530

    Armstrong Number

---
+++ Number_System/Number_System/Armstrong.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_System
+{
+    internal class Armstrong
+    {
+        public static void Main()
+        {
+            int originalValue = 153;
+            int tempValue = originalValue;
+            int digitCount = 0;
+            int remainder;
+            int poweredDigit;
+            int armstrongSum = 0;
+
+            while (tempValue > 0)
+            {
+                tempValue = tempValue / 10;
+                digitCount++;
+            }
+
+            tempValue = originalValue;
+
+            while (tempValue > 0)
+            {
+                remainder = tempValue % 10;
+                poweredDigit = (int)Math.Pow(remainder, digitCount);
+                armstrongSum = armstrongSum + poweredDigit;
+                tempValue = tempValue / 10;
+
+            }
+            if (originalValue == armstrongSum)
+            {
+                Console.WriteLine(originalValue + " is Armstrong");
+            }
+            else
+            {
+                Console.WriteLine(originalValue + " is not Armstrong");
+            }
+        }
+    }
+}
---
+++ Number_System/Number_System/ArmstrongNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_System
+{
+    internal class ArmstrongNumbers
+    {
+        public static void Main()
+        {
+            for (int i = 1; i <= 1000; i++)
+            {
+                int originalValue = i;
+                int tempValue = originalValue;
+                int digitCount = 0;
+                int remainder;
+                int poweredDigit;
+                int armstrongSum = 0;
+
+                while (tempValue > 0)
+                {
+                    tempValue = tempValue / 10;
+                    digitCount++;
+                }
+
+                tempValue = originalValue;
+
+                while (tempValue > 0)
+                {
+                    remainder = tempValue % 10;
+                    poweredDigit = (int)Math.Pow(remainder, digitCount
+                        );
+                    armstrongSum = armstrongSum + poweredDigit;
+                    tempValue = tempValue / 10;
+                }
+
+                if (armstrongSum == originalValue)
+                {
+                    Console.WriteLine(originalValue + " is Armstrong Number");
+                }
+            }
+        }
+    }
+}
