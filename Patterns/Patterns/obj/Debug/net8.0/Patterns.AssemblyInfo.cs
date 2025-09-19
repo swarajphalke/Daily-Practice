8d68cb3457737cb8c7e6de4bf83f61516f75f77e
Swaraj Phalke <swarajphalke555@gmail.com>
2025-09-19 20:00:26 +0530

    updated

--- Patterns/Patterns/obj/Debug/net8.0/Patterns.AssemblyInfo.cs
+++ Patterns/Patterns/obj/Debug/net8.0/Patterns.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fad2dcb1dcb6259ca6bbb3fa1f8ba9bd9914de8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655487291f6d833298b248e5504f0a7d14f51128")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patterns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
